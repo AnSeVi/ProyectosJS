fc64ed60c89ad56fc050c5523b40405d20c61d90
Andres Vivanco <vivancosebastian218@gmail.com>
2022-01-06 11:12:04 -0500

    finalizacion de la pantalla mobile del home

--- interactive.js
+++ interactive.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const carouselElement = document.querySelectorAll('.carousel');
     M.Carousel.init(carouselElement, {
-        duration: 150
+        duration: 150,
+        dist: 5,
+        shift: 10,
+        padding: 0,
+        numVisible: 3,
+        indicators: true
     });
 });
